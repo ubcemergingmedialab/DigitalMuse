c8a2bfd1ad9a7436c394cb0b40ead30a9efa8e13
Olivia Chen <ochen@Olivias-MBP.hitronhub.home>
2021-08-27 13:43:12 -0700

    theme.html completely fixed

--- public/theme.js
+++ public/theme.js
@@ -1,3 +1,13 @@
 ThemeHandler = function (name) {
     document.getElementById("link").setAttribute("href", "watch.html?theme=" + name)
 }
+
+HighlightHandler = function(identifier) {
+    let borders = document.querySelectorAll(".image-border");
+    for(let element of borders) {
+        console.log("removing active " + element.classList)
+        element.classList.remove("active");
+    }
+    document.querySelector(identifier).classList.add("active");
+    console.log("adding active "+ document.querySelector(identifier).classList)
+}
